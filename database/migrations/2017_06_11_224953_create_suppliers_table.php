20f5ef6dc2fb8653dfdef8165d50a2f067274b76
w1lldone <wildan.arrahman@gmail.com>
2017-06-12 06:20:47 +0700

    add styles and tables

--- database/migrations/2017_06_11_224953_create_suppliers_table.php
+++ database/migrations/2017_06_11_224953_create_suppliers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateSuppliersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('suppliers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('photo', 250)->nullable();
-            $table->string('email')->unique();
+            $table->string('name', 150);
             $table->string('address', 250)->nullable();
             $table->string('contact', 15)->nullable();
-            $table->string('password');
-            $table->integer('privilage_id')->unsigned();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('suppliers');
     }
 }
