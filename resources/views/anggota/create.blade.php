cadf2dd44d09cc991cdf0136fd9479ce1ae5c428
w1lldone <wildan.arrahman@gmail.com>
2017-06-13 22:49:50 +0700

    update login pages

---
+++ resources/views/anggota/create.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.master')
+
+@section('sidebar')
+	@include('layouts.sidebar')
+@endsection
+
+@section('content')
+<!-- Page Header-->
+<header class="page-header">
+  <div class="container-fluid">
+    <h2 class="no-margin-bottom">Anggota</h2>
+  </div>
+</header>
+<!-- Breadcrumb-->
+<ul class="breadcrumb">
+  <div class="container-fluid">
+    <li class="breadcrumb-item"><a href="/anggota">Anggota</a></li>
+    <li class="breadcrumb-item active">Tambah anggota</li>
+  </div>
+</ul>
+
+<div class="content-wrapper">
+	<!-- TABLES SECTION  -->
+	<section class="forms">
+		<div class="container-fluid">
+			<div class="row">
+				{{-- HORIZONTAL FORM --}}
+				<div class="col-md-8 offset-md-2">
+					<div class="card">
+					  <div class="card-header d-flex align-items-center">
+					    <h3 class="h4">Form tambah anggota</h3>
+					  </div>
+					  <div class="card-body">
+					    <form class="form-horizontal" method="POST" action="/anggota">
+					      {{ csrf_field() }}
+					      {{-- INPUT NAME --}}
+					      <div class="form-group row">
+					        <label class="col-sm-3 form-control-label">Nama</label>
+					        <div class="col-sm-9">
+					          <input name="name" id="inputHorizontalSuccess" type="text" placeholder="Nama lengkap" class="form-control form-control-success {{ $errors->has('name') ? ' has-error' : '' }}" required>
+					          @if ($errors->has('name'))
+						          <small class="form-text text-danger">{{ $errors->first('name') }}</small>
+					          @endif
+					        </div>
+					      </div>
+					      {{-- INPUT EMAIL --}}
+					      <div class="form-group row">
+					        <label class="col-sm-3 form-control-label">Email</label>
+					        <div class="col-sm-9">
+					          <input name="email" id="inputHorizontalSuccess" type="email" placeholder="email@contoh.com" class="form-control form-control-success {{ $errors->has('email') ? ' has-error' : '' }}" required>
+					          @if ($errors->has('email'))
+						          <small class="form-text text-danger">{{ $errors->first('email') }}</small>
+					          @endif
+					        </div>
+					      </div>
+					      {{-- INPUT ADDRESS --}}
+					      <div class="form-group row">
+					        <label class="col-sm-3 form-control-label">Alamat</label>
+					        <div class="col-sm-9">
+					          <input name="address" id="inputHorizontalSuccess" type="text" placeholder="Alamat lengkap" class="form-control form-control-success {{ $errors->has('address') ? ' has-error' : '' }}" required>
+					          @if ($errors->has('address'))
+						          <small class="form-text text-danger">{{ $errors->first('address') }}</small>
+					          @endif
+					        </div>
+					      </div>
+					      {{-- INPUT CONTACT --}}
+					      <div class="form-group row">
+					        <label class="col-sm-3 form-control-label">No Hp</label>
+					        <div class="col-sm-9">
+					          <input name="contact" id="inputHorizontalSuccess" type="text" placeholder="0812306*****" class="form-control form-control-success {{ $errors->has('contact') ? ' has-error' : '' }}" required>
+					          @if ($errors->has('contact'))
+						          <small class="form-text text-danger">{{ $errors->first('contact') }}</small>
+					          @endif
+					        </div>
+					      </div>
+					      {{-- INPUT PASSWORD --}}
+					      <div class="form-group row">
+					        <label class="col-sm-3 form-control-label">Password</label>
+					        <div class="col-sm-9">
+					          <input name="password" id="inputHorizontalSuccess" type="password" placeholder="password" class="form-control form-control-success {{ $errors->has('password') ? ' has-error' : '' }}" required>
+					          @if ($errors->has('password'))
+						          <small class="form-text text-danger">{{ $errors->first('password') }}</small>
+					          @endif
+					        </div>
+					      </div>
+					      {{-- INPUT PASSWORD CONFIRMATION --}}
+					      <div class="form-group row">
+					        <label class="col-sm-3 form-control-label">Ulangi password</label>
+					        <div class="col-sm-9">
+					          <input name="password_confirmation" id="inputHorizontalSuccess" type="password" placeholder="Ulangi password" class="form-control form-control-success" required>
+					        </div>
+					      </div>
+					      {{-- INPUT PRIVILAGES --}}
+					      <div class="form-group row">
+					        <label class="col-sm-3 form-control-label">Kewenangan</label>
+					        <div class="col-sm-9">
+					          <select name="privilage_id" class="form-control">
+                                @foreach ($privilages as $privilage)
+                                	<option value="{{ $privilage->id }}">{{ $privilage->name }}</option>
+                                @endforeach
+                              </select>
+					        </div>
+					      </div>
+
+					      <div class="form-group row">       
+					        <div class="col-sm-9 offset-sm-3">
+					          <input type="submit" value="Signin" class="btn btn-primary">
+					        </div>
+					      </div>
+					    </form>
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
+
+@endsection
---
+++ resources/views/layouts/master-plain.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sistem Informasi Pengendalian Kedelai Lokal</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <!-- Google fonts - Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,700">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="{{ asset('css/style.default.css') }}" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <!-- Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/img/favicons/manifest.json">
+    <link rel="mask-icon" href="/img/favicons/safari-pinned-tab.svg" color="#49525f">
+    <link rel="shortcut icon" href="/img/favicons/favicon.ico">
+    <meta name="msapplication-config" content="/img/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <!-- Font Awesome CDN-->
+    <!-- you can replace it by local Font Awesome-->
+    <script src="https://use.fontawesome.com/99347ac47f.js"></script>
+    <!-- Font Icons CSS-->
+    <link rel="stylesheet" href="{{ asset('icons-reference/styles.css') }}">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    @yield('content')
+    <!-- Javascript files-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="{{ asset('js/tether.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.cookie.js') }}"> </script>
+    <script src="{{ asset('js/jquery.validate.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+    <script src="{{ asset('js/charts-home.js') }}"></script>
+    <script src="{{ asset('js/front.js') }}"></script>
+  </body>
+</html>
