5b272d1728bae72e1b238fe877d6689ec7f84978
w1lldone <wildan.arrahman@gmail.com>
2017-06-15 06:40:39 +0700

    onfarm model

---
+++ database/migrations/2017_06_14_232847_create_onfarms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOnfarmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('onfarms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', '250');
+            $table->integer('user_id')->unsigned();
+            $table->integer('seed_id')->unsigned()->nullable();
+            $table->integer('seed_quantity')->nullable();
+            $table->integer('area')->nullable();
+            $table->timestamp('planted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('onfarms');
+    }
+}
