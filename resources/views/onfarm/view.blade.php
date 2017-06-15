72ee29509939f90917f91aaa91ddde91755d4c7e
w1lldone <wildan.arrahman@gmail.com>
2017-06-16 05:19:42 +0700

    fix onfarm view

--- resources/views/onfarm/view.blade.php
+++ resources/views/onfarm/view.blade.php
@@ -28,24 +28,40 @@
                 <div class="project-title d-flex align-items-center pt-4 py-4">
                   {{-- <div class="image has-shadow"><img src="/img/project-1.jpg" alt="..." class="img-fluid"></div> --}}
                   <div class="text">
-                    <h3 class="h4">Tanam kedelai bulan juli</h3><small>Lorem Ipsum Dolor</small>
+                    <h3 class="h4">Tanam kedelai bulan juli</h3><small>Muhammad Alahudin, Kelompok Tani makmur</small>
                   </div>
                 </div>
-                <div class="project-date"><span class="hidden-sm-down">Today at 4:24 AM</span></div>
+                {{-- <div class="project-date"><span class="hidden-sm-down">Today at 4:24 AM</span></div> --}}
               </div>
               <div class="statistic col-lg-4 clearfix align-items-center" style="margin-bottom: inherit;">
-               <div class="icon bg-green float-left"><i class="fa fa-line-chart"></i></div>
-               <div class="text text-right float-right"><strong>700 Kg</strong><br><small>Benih digunakan</small></div>
+              	@isset ($seed)
+	               <div class="icon bg-green float-left"><i class="fa fa-line-chart"></i></div>
+	               <div class="text text-right float-right"><strong>700 Kg</strong><br><small>Benih digunakan</small></div>
+              	@endisset
+              	@empty ($seed)
+                   <div class="text text-center">
+    	               <h3 class="light-text">Benih belum dibeli</h3>
+    	               <a class="round-link" href="/seed/create/1">Beli</a>
+                   </div>
+              	@endempty
               </div>
               <div class="statistic col-lg-4 clearfix align-items-center" style="border-right: none; margin-bottom: inherit;">
-               <div class="icon bg-orange float-left"><i class="fa fa-calendar-o"></i></div>
-               <div class="text text-right float-right"><small>Tanggal tanam</small><br><strong>27 Mei, 2017</sup></strong></div>
+              	@isset ($seed)
+	               <div class="icon bg-orange float-left"><i class="fa fa-calendar-o"></i></div>
+	               <div class="text text-right float-right"><small>Tanggal tanam</small><br><strong>27 Mei, 2017</sup></strong></div>
+              	@endisset
+              	@empty ($seed)
+                   <div class="text text-center">
+    	               <h3 class="light-text">Benih belum ditanam</h3>
+    	               <a class="round-link bg-orange btn disabled" href="/plant/create/1">tanam</a>
+                   </div>
+              	@endempty
               </div>
-
             </div>
           </div>
 	    </div>
     </section>
+
 	<section class="dashboard-header">
       <div class="container-fluid">
         <div class="row">
@@ -162,14 +178,19 @@
             </div>
           </div>
           <div class="chart col-lg-3 col-12">
-            <!-- Bar Chart   -->
+            <!-- TOTAL BIAYA   -->
             <div class="bar-chart has-shadow bg-white pb-3">
               <div class="title"><strong class="text-violet">Rp. 553.000</strong><br><small>Total biaya pra-panen</small></div>
             </div>
-            <!-- Numbers-->
+            <!-- BELUM PANEN-->
             <div class="statistic d-flex align-items-center bg-white has-shadow">
               <div class="icon bg-green"><i class="fa fa-line-chart"></i></div>
-              <div class="text"><strong>99.9%</strong><br><small>Success Rate</small></div>
+              <div class="text"><a href="/panen/create/1" style="text-decoration: none;"><b>Belum panen</b><br><small>Klik untuk panen</small></a></div>
+            </div>
+            <!-- SUDAH PANEN-->
+            <div class="statistic d-flex align-items-center bg-white has-shadow">
+              <div class="icon bg-green"><i class="fa fa-line-chart"></i></div>
+              <div class="text"><a href="/panen/create/1" style="text-decoration: none;"><b>Panen pada:</b><br><small>30 Agustus, 2017</small></a></div>
             </div>
           </div>
         </div>
