c78a0e9654e0909a627b526d3255c91136fe6e79
w1lldone <wildan.arrahman@gmail.com>
2017-06-15 16:42:50 +0700

    update view

--- resources/views/onfarm/view.blade.php
+++ resources/views/onfarm/view.blade.php
@@ -24,24 +24,20 @@
 	    <div class="container-fluid">
     	  <div class="project">
             <div class="row bg-white has-shadow">
-              <div class="left-col col-lg-6 d-flex align-items-center justify-content-between">
+              <div class="col-lg-4 d-flex align-items-center justify-content-between">
                 <div class="project-title d-flex align-items-center">
-                  <div class="image has-shadow"><img src="/img/project-1.jpg" alt="..." class="img-fluid"></div>
+                  {{-- <div class="image has-shadow"><img src="/img/project-1.jpg" alt="..." class="img-fluid"></div> --}}
                   <div class="text">
                     <h3 class="h4">Tanam kedelai bulan juli</h3><small>Lorem Ipsum Dolor</small>
                   </div>
                 </div>
                 <div class="project-date"><span class="hidden-sm-down">Today at 4:24 AM</span></div>
               </div>
-              <div class="right-col col-lg-6 d-flex align-items-center">
-                <div class="time"><i class="fa fa-clock-o"></i>12:00 PM </div>
-                <div class="comments"><i class="fa fa-comment-o"></i>20</div>
-                <div class="project-progress">
-                  <div class="progress">
-                    <div role="progressbar" style="width: 45%; height: 6px;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" class="progress-bar bg-red"></div>
-                  </div>
-                </div>
+              <div class="statistic col-lg-4 clearfix align-items-center">
+               <div class="icon bg-green float-left"><i class="fa fa-line-chart"></i></div>
+               <div class="text float-right"><strong>99.9%</strong><br><small>Total</small></div>
               </div>
+
             </div>
           </div>
 	    </div>
