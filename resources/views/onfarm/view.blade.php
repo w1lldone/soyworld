c32d96480892ebe7497f7c7185221f28bfeb318e
w1lldone <wildan.arrahman@gmail.com>
2017-06-18 05:40:20 +0700

    update onfarm view

--- resources/views/onfarm/view.blade.php
+++ resources/views/onfarm/view.blade.php
@@ -24,17 +24,32 @@
 	    <div class="container-fluid">
     	  <div class="project">
         @include('layouts.alerts')
+            {{-- PORJECT SECTION --}}
+            <div class="row bg-white has-shadow mb-2 p-3">
+              <div class="left-col col-lg-6 d-flex align-items-center justify-content-between">
+                <div class="project-title d-flex align-items-center">
+                  <div class="image has-shadow"><img src="/img/project-1.jpg" alt="..." class="img-fluid"></div>
+                  <div class="text">
+                    <h3 class="h4">{{ $onfarm->name }}</h3><small>{{ $onfarm->user->name }}</small>
+                  </div>
+                </div>
+                <div class="project-date"><span class="hidden-sm-down">dibuat: {{ $onfarm->created_at->toFormattedDateString() }}</span></div>
+              </div>
+              <div class="right-col col-lg-6 d-flex align-items-center justify-content-between" style="border-right: none;">
+                  <h4 class="my-0">Total biaya pra-panen:</h4>
+                  <h4 class="my-0"><span class="badge badge-info" style="font-size: inherit;">Rp. 503.000</span></h4>
+              </div>
+            </div>
+            {{-- DATA SECTION --}}
             <div class="row bg-white has-shadow" style="padding: 5px 15px;">
-              <div class="col-lg-4 d-flex align-items-center justify-content-between">
+              {{-- <div class="col-lg-4 d-flex align-items-center justify-content-between">
                 <div class="project-title d-flex align-items-center pt-4 py-4">
-                  {{-- <div class="image has-shadow"><img src="/img/project-1.jpg" alt="..." class="img-fluid"></div> --}}
                   <div class="text">
                     <h3 class="text-light">{{ $onfarm->name }}</h3><small>{{ $onfarm->user->name }}, {{ $onfarm->user->poktan->name }}</small>
                   </div>
                 </div>
-                {{-- <div class="project-date"><span class="hidden-sm-down">Today at 4:24 AM</span></div> --}}
-              </div>
-              <div class="statistic col-lg-4 clearfix align-items-center" style="margin-bottom: inherit;">
+              </div> --}}
+              <div class="statistic col-sm-4 clearfix align-items-center" style="margin-bottom: inherit;">
               	@isset ($onfarm->seed)
 	               <div class="icon bg-green float-left"><i class="fa fa-line-chart"></i></div>
 	               <div class="text text-right float-right"><strong>{{ $onfarm->seed->quantity }} Kg</strong><br><small>Benih digunakan</small></div>
@@ -46,19 +61,36 @@
                    </div>
               	@endempty
               </div>
-              <div class="statistic project col-lg-4 clearfix align-items-center" style="border-right: none; margin-bottom: inherit;">
-              	@isset ($onfarm->planted_at)
-	               <div class="icon bg-orange float-left"><i class="fa fa-calendar-o"></i></div>
-	               <div class="text text-right float-right"><small>Tanggal tanam</small><br><strong>27 Mei, 2017</sup></strong></div>
-              	@endisset
-              	@empty ($onfarm->planted_at)
+              <div class="statistic project col-sm-4 clearfix align-items-center" style="margin-bottom: inherit;">
+                @isset ($onfarm->planted_at)
+                 <div class="icon bg-orange float-left"><i class="fa fa-calendar-o"></i></div>
+                 <div class="text text-right float-right"><small>Tanggal tanam</small><br><strong>27 Mei, 2017</sup></strong></div>
+                @endisset
+                @empty ($onfarm->planted_at)
                    <div class="text text-center">
-    	               <h3 class="text-light">Benih belum ditanam</h3>
+                     <h3 class="text-light">Benih belum ditanam</h3>
                      @isset ($onfarm->seed)
-      	               <a class="round-link bg-orange" href="/plant/create/1">tanam</a>
+                       <a class="round-link bg-orange" href="/plant/create/1">tanam</a>
                      @endisset
                      @empty ($onfarm->seed)
                          <small>Lakukan pembelian benih dulu</small>
+                     @endempty
+                   </div>
+                @endempty
+              </div>
+              <div class="statistic project col-sm-4 clearfix align-items-center" style="margin-bottom: inherit; border-right: none;">
+              	@isset ($onfarm->harvest)
+	               <div class="icon bg-orange float-left"><i class="fa fa-calendar-o"></i></div>
+	               <div class="text text-right float-right"><small>Tanggal tanam</small><br><strong>27 Mei, 2017</sup></strong></div>
+              	@endisset
+              	@empty ($onfarm->harvest)
+                   <div class="text text-center">
+    	               <h3 class="text-light">Kedelai belum dipanen</h3>
+                     @isset ($onfarm->planted_at)
+      	               <a class="round-link bg-info" href="/plant/create/1">Panen</a>
+                     @endisset
+                     @empty ($onfarm->planted_at)
+                         <small>Lakukan penanaman dulu</small>
                      @endempty
                    </div>
               	@endempty
