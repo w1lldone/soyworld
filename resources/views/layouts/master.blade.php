8eefa43af720646d04ae501a77732a15b1272424
w1lldone <wildan.arrahman@gmail.com>
2017-06-21 11:44:16 +0700

    remove garden icons

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -31,6 +31,7 @@
     <script src="https://use.fontawesome.com/99347ac47f.js"></script>
     <!-- Font Icons CSS-->
     <link rel="stylesheet" href="{{ asset('icons-reference/styles.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('garden-icons/styles.css') }}"> --}}
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
