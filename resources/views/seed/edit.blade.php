4d7e53800353faf3e6293bfe2eadc0c90a758a29
w1lldone <wildan.arrahman@gmail.com>
2017-06-22 11:40:56 +0700

    add custom icon to input

--- resources/views/seed/edit.blade.php
+++ resources/views/seed/edit.blade.php
@@ -43,7 +43,7 @@
 							          <form method="POST" class="pr-2" action="/seedphoto/{{ $photo->id }}" enctype="multipart/form-data">
 							          	  {{ csrf_field() }}
 								          <input onclick="return confirm('Foto akan langsung diganti. Apa anda yakin?')" name="photo" type="file" class="inputfile inputfile-warning" id="file-1" onchange="this.form.submit()">
-								          <label for="file-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg> <span style="color: inherit;">Ubah foto&hellip;</span></label>
+								          <label for="file-1"><i class="fa fa-edit"></i><span style="color: inherit;">Ubah foto&hellip;</span></label>
 							          </form>
 							          <form method="POST" action="/seedphoto/{{ $photo->id }}">
 							        		{{ csrf_field() }}
@@ -58,7 +58,7 @@
 					        		{{ csrf_field() }}
 					        		<input type="hidden" name="seed_id" value="{{ $seed->id }}">
 					        		<input name="photo" id="file-2" type="file" class="inputfile inputfile-1" onchange="this.form.submit()">
-					        		<label for="file-2"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg> <span style="color: inherit;">Tambah foto&hellip;</span></label>
+					        		<label for="file-2"><i class="fa fa-upload"></i> <span style="color: inherit;">Tambah foto&hellip;</span></label>
 					        	</form>
 					        </div>
 					      </div>
