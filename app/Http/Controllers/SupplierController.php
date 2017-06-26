66baa33c6474ebf1c9d8fd2977010d5e68fd16fc
w1lldone <wildan.arrahman@gmail.com>
2017-06-26 21:38:06 +0700

    add supplier store

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Supplier;
 
 class SupplierController extends Controller
@@ -12,6 +13,17 @@ class SupplierController extends Controller
 		$this->middleware('auth');
 	}
 
+    public function validator($request)
+    {
+        return Validator::make($request, [
+            'name' => 'required',
+            'description' => 'required',
+            'address' => 'required',
+            'contact' => 'required',
+            'poktan_id' => 'required|exists:poktans,id',
+        ]);
+    }
+
     public function create()
     {
         return view('supplier.create');
@@ -20,10 +32,14 @@ class SupplierController extends Controller
     public function store(Request $request)
     {
         if (!$request->has('poktan_id')) $request->poktan_id = auth()->user()->poktan->id;
+        
+        $this->validator($request->all())->validate();
 
     	$supplier = Supplier::addSupplier($request);
 
-    	return redirect("$request->url");
+        $url = $request->has('url') ? $request->url : "/supplier" ;
+
+        return redirect($url)->with('success', 'Berhasil menambah suppler!');
     }
 
     public function index()
