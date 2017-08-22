e4c41f278e48bcd59d6d41f5290a372f4adf2a27
w1lldone <wildan.arrahman@gmail.com>
2017-08-22 16:58:39 +0700

    update database seeder

--- database/seeds/PrivilageSeeder.php
+++ database/seeds/PrivilageSeeder.php
@@ -12,21 +12,23 @@ class PrivilageSeeder extends Seeder
     public function run()
     {
         DB::table('privilages')->insert([
-        	'id' => 1,
-            'name' => 'admin',
-            'is_superadmin' => 1,
-        ],[
-        	'id' => 2,
-            'name' => 'petani',
-            'is_superadmin' => 0,
-        ],[
-    		'id' => 3,
-    	    'name' => 'instansi',
-    	    'is_superadmin' => 0,
-        ],[
-        	'id' => 4,
-            'name' => 'industri',
-            'is_superadmin' => 0,
+	    	[
+	        	'id' => 1,
+	            'name' => 'admin',
+	            'is_superadmin' => 1,
+	        ],[
+	        	'id' => 2,
+	            'name' => 'petani',
+	            'is_superadmin' => 0,
+	        ],[
+	    		'id' => 3,
+	    	    'name' => 'instansi',
+	    	    'is_superadmin' => 0,
+	        ],[
+	        	'id' => 4,
+	            'name' => 'industri',
+	            'is_superadmin' => 0,
+	        ]
         ]);
     }
 }
