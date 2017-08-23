dd168a6ab2b09c2630822ffb9b525332c4450a42
w1lldone <wildan.arrahman@gmail.com>
2017-08-23 23:56:25 +0700

    add onfarm relation

--- app/Harvest.php
+++ app/Harvest.php
@@ -10,6 +10,19 @@ class Harvest extends Model
 	public function postharvest(){
 		return $this->hasMany('App\Postharvest');
 	}
+
+	public function onfarm(){
+		return $this->belongsTo('App\Onfarm');
+	}
+
+	public function addPostharvest($request)
+	{
+		return $this->postharvest()->create([
+			'name' => 'panen '.$this->onfarm->name,
+			'weight' => $request->weight,
+		]);
+	}
     
     protected $guarded = ['id'];
+    protected $dates = ['harvested_at'];
 }
