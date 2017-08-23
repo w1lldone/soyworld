b974a0207f677bcfdcfd43d08b5d300e6a48e5fe
w1lldone <wildan.arrahman@gmail.com>
2017-08-23 23:55:25 +0700

    fix validation fail views

--- resources/views/harvest/create.blade.php
+++ resources/views/harvest/create.blade.php
@@ -22,6 +22,7 @@
 			<div class="row">
 				{{-- HORIZONTAL FORM --}}
 				<div class="col-md-8 offset-md-2">
+					@include('layouts.alerts')
 					<div class="card">
 					  <div class="card-header d-flex align-items-center">
 					    <h3 class="h4">Form panen kedelai</h3>
@@ -30,7 +31,7 @@
 					    <form class="form-horizontal" method="POST" action="/harvest">
 					      {{ csrf_field() }}
 				      	{{-- INPUT ONFARM --}}
-  				      <div class="form-group row">
+  				      <div class="form-group row {{ $errors->has('onfarm_id') ? ' has-danger' : '' }}">
   					      <label class="col-sm-3 form-control-label">Penanaman</label>
   					      <div class="col-sm-9">
 						      @if (request()->has('onfarm_id'))
@@ -39,6 +40,7 @@
 										</select>
 					      	@else
 					          <select name="onfarm_id" class="form-control">
+					          	<option></option>
 					          	@if (auth()->user()->isSuperadmin())
 					          		@foreach (\App\Onfarm::doesntHave('harvest')->get() as $onfarm)
 	                      	<option value="{{ $onfarm->id }}">{{ $onfarm->name }}</option>
@@ -49,24 +51,27 @@
 	                      @endforeach
 					          	@endif
                     </select>
+                    @if ($errors->has('onfarm_id'))
+          	          <small class="form-text text-danger">{{ $errors->first('onfarm_id') }}</small>
+                    @endif
 					        </div>
 						    </div>
 					      @endif
 					      {{-- INPUT DATE --}}
-					      <div class="form-group row">
+					      <div class="form-group row {{ $errors->has('harvested_at') ? ' has-danger' : '' }}">
   					      <label class="col-sm-3 form-control-label">Tanggal</label>
   					      <div class="col-sm-9">
   					      	<div class="input-group">
-		  					      <input value="{{ old('date') }}" data-provide="datepicker" type="text" placeholder="Tanggal panen" name="planted_at" class="form-control datepicker" required>
+		  					      <input value="{{ old('harvested_at') }}" data-provide="datepicker" type="text" placeholder="Tanggal panen" name="harvested_at" class="form-control datepicker" required>
 		  					      <span class="input-group-addon""><i class="fa fa-calendar"></i></span>
-			                @if ($errors->has('date'))
-			      	          <small class="form-text text-danger">{{ $errors->first('date') }}</small>
+			                @if ($errors->has('harvested_at'))
+			      	          <small class="form-text text-danger">{{ $errors->first('harvested_at') }}</small>
 			                @endif
   					      	</div>
   					      </div>
 	  					  </div>
 	  					  {{-- INPUT WEIGHT --}}
-					      <div class="form-group row">
+					      <div class="form-group row  {{ $errors->has('weight') ? ' has-danger' : '' }}">
   					      <label class="col-sm-3 form-control-label">Jumlah Kedelai</label>
   					      <div class="col-sm-9">
   					      	<div class="input-group">
