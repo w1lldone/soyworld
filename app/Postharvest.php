983ddde1e1146fb16b8bc37452f9c82f4f414132
w1lldone <wildan.arrahman@gmail.com>
2017-08-23 22:44:32 +0700

    add postharvest and harvest models and migrations

---
+++ app/Postharvest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Postharvest extends Model
+{
+    //
+}
---
+++ database/migrations/2017_08_23_045731_create_harvests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHarvestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('harvests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('onfarm_id')->unsigned();
+            $table->date('harvested_at')->nullable();
+            $table->boolean('sold')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('harvests');
+    }
+}
