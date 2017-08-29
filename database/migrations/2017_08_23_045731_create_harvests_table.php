3ec40343ec6284f0a222ec66de586ab7995b55a5
w1lldone <wildan.arrahman@gmail.com>
2017-08-29 20:27:51 +0700

    modify harvest table

--- database/migrations/2017_08_23_045731_create_harvests_table.php
+++ database/migrations/2017_08_23_045731_create_harvests_table.php
@@ -17,7 +17,9 @@ class CreateHarvestsTable extends Migration
             $table->increments('id');
             $table->integer('onfarm_id')->unsigned();
             $table->date('harvested_at')->nullable();
-            $table->boolean('sold')->nullable();
+            $table->integer('initial_stock')->nullable();
+            $table->integer('ending_stock')->nullable();
+            $table->boolean('sold')->default(0);
             $table->timestamps();
         });
     }
