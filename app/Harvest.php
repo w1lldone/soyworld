d75777b37e291ae66e5613edd59c3aabc3a9e477
w1lldone <wildan.arrahman@gmail.com>
2017-08-30 11:28:03 +0700

    add on sale and total stock

--- app/Harvest.php
+++ app/Harvest.php
@@ -27,6 +27,20 @@ class Harvest extends Model
 	{
 		return $this->on_sale ? 'Dijual' : 'Tidak dijual';
 	}
+
+	public static function totalStock()
+	{
+		$stock = auth()->user()->isSuperadmin() ? Harvest::all()->sum('ending_stock') : auth()->user()->harvest->sum('ending_stock');
+
+		return $stock;
+	}
+
+	public static function onSaleStock()
+	{
+		$stock = auth()->user()->isSuperadmin() ? Harvest::where('on_sale', 1)->get()->sum('ending_stock') : auth()->user()->harvest()->where('on_sale', 1)->get()->sum('ending_stock');
+
+		return $stock;
+	}
     
     protected $guarded = ['id'];
     protected $dates = ['harvested_at'];
