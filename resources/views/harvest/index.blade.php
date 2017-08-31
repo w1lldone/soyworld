f47e49012ef75e347bee1a1e8b1aed6a53e124b7
w1lldone <wildan.arrahman@gmail.com>
2017-08-31 08:20:16 +0700

    update harvest index

--- resources/views/harvest/index.blade.php
+++ resources/views/harvest/index.blade.php
@@ -82,28 +82,7 @@
                       <td>
                         @if (empty($harvest->ending_stock))
                           <button data-toggle="modal" data-target="#tambahStok{{$harvest->id}}" class="round-link btn">Tambah stok</button>
-                          <div class="modal fade" id="tambahStok{{$harvest->id}}">
-                            <div class="modal-dialog" role="document">
-                              <div class="modal-content">
-                                <div class="modal-body">
-                                  <h2>Tambah stok</h2>
-                                  <form action="/stock" method="POST">
-                                    {{ csrf_field() }}
-                                    <div class="row">
-                                      <div class="col-12 form-group input-group">
-                                        <input name="quantity" type="number" class="form-control" placeholder="Masukkan stok" aria-describedby="basic-addon1">
-                                        <span class="input-group-addon" id="basic-addon1">Kg</span>
-                                        <input type="hidden" name="harvest_id" value="{{ $harvest->id }}">
-                                      </div>
-                                      <div class="col-12 clearfix">
-                                        <button type="submit" class="btn btn-primary float-right">Simpan</button>
-                                      </div>
-                                    </div>
-                                  </form>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
+                          @include('harvest.modal')
                         @else
                           {{ $harvest->ending_stock }}
                         @endif
@@ -117,7 +96,6 @@
   	    </div>
   	  </div>
   	</div>
-  	
   </section>
 </div>
 @endsection
