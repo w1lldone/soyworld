85f2a6e2bd30b0c45d1b9343f5881bb927cd6a41
w1lldone <wildan.arrahman@gmail.com>
2017-09-02 19:14:26 +0700

    add relation and status colour method

--- app/Price.php
+++ app/Price.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Price extends Model
 {
-    //
+    public static function latestPrice()
+    {
+    	return number_format(Price::latest()->first()->nominal, 0, ',', '.');
+    }
 }
