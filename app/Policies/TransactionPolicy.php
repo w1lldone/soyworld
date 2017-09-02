7d950d35cce473c2d14ab40717885d6ebf554619
w1lldone <wildan.arrahman@gmail.com>
2017-09-02 14:56:15 +0700

    add transaction policy

---
+++ app/Policies/TransactionPolicy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Transaction;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TransactionPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the transaction.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Transaction  $transaction
+     * @return mixed
+     */
+    public function view(User $user, Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create transactions.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->privilage_id == 4;
+    }
+
+    /**
+     * Determine whether the user can update the transaction.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Transaction  $transaction
+     * @return mixed
+     */
+    public function update(User $user, Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the transaction.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Transaction  $transaction
+     * @return mixed
+     */
+    public function delete(User $user, Transaction $transaction)
+    {
+        //
+    }
+}
