7b491c46adc7e02ba201e8fdb4d43bd63b843e6a
w1lldone <wildan.arrahman@gmail.com>
2017-09-02 19:14:53 +0700

    add relation and status colour method

--- app/Status.php
+++ app/Status.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    //
+	/*RELATION METHOD*/
+    public function transaction(){
+    	return $this->hasMany('App\Transaction');
+    }
+
+    /*CUSTOM METHOD*/
+
+    public function getStatusColorAttribute()
+    {
+    	switch ($this->id) {
+    		case 1:
+    			return 'warning';
+    		break;
+    		
+    		case 2:
+    			return 'info';	
+			break;
+
+			case 3:
+				return 'success';
+			break;
+
+			case 4:
+				return 'danger';
+			break;
+    	}
+    }
+
+    protected $guarded = ['id'];
 }
