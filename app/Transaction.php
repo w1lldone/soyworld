e5821d34433e8d2bad5645f1a69703631ff3b4bb
w1lldone <wildan.arrahman@gmail.com>
2017-09-02 14:57:51 +0700

    add relation and newTransaction method

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,50 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    /**
+    * RELATION SECTION
+    */
+    public function transaction_detail(){
+    	return $this->hasMany('App\TransactionDetail');
+    }
+
+    public function addDetail($harvestId, $quantity)
+    {
+    	return $this->transaction_detail()->create([
+    		'harvest_id' => $harvestId,
+    		'quantity' => $quantity,
+    		'price' => \App\Price::latest()->first()->nominal,
+		]);
+    }
+
+    /**
+	* CUSTOM METHOD SECTION
+    */
+
+    public static function newTransaction($request)
+    {
+    	$transaction = Transaction::create([
+    		'user_id' => auth()->id(),
+    		'status_id' => 1,
+    		'delivered_to' => $request->delivered_to, 
+		]);
+
+		foreach (\App\Harvest::readyStock() as $stock) {
+			if ($request->quantity > $stock->ending_stock) {
+				$transaction->addDetail($stock->id, $stock->ending_stock);
+				$request->quantity -= $stock->ending_stock;
+				$stock->update([
+					'ending_stock' => 0,
+				]);
+			} else {
+				$transaction->addDetail($stock->id, $request->quantity);
+				$stock->ending_stock -= $request->quantity;
+				$stock->save();
+
+				return $transaction->load('transaction_detail.harvest');
+			}
+		}
+    }
+
+    protected $guarded = ['id'];
 }
