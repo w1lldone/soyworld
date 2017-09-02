3fc7ba316efcd6930351b53eefc226d554d7630a
w1lldone <wildan.arrahman@gmail.com>
2017-09-02 13:17:46 +0700

    fix index soybean industri view

--- resources/views/soybean/index.blade.php
+++ resources/views/soybean/index.blade.php
@@ -25,10 +25,11 @@
 	    <div class="row">
 	    	<div class="col-12">
 			    @include('soybean.navpill')
-			    <div class="my-4 clearfix d-flex justify-content-between align-items-center">
-				    <h2 class="m-0 px-1"><span class="badge badge-primary p-2">Stok <b>{{ \App\Harvest::where('on_sale', 1)->get()->sum('ending_stock') }} Kg</b></span></h2>
-				    <h2 class="m-0 px-1"><span class="badge badge-success p-2">Harga <b>Rp. 8000/Kg</b></span></h2>
-				    <button class="btn btn-info ml-auto"><i class="fa fa-plus"></i> Pesan kedelai</button>
+			    <div class="my-4 clearfix d-flex flex-wrap justify-content-between align-items-center">
+				    <h2 class="px-1"><span class="badge badge-primary p-2">Stok <b>{{ \App\Harvest::where('on_sale', 1)->get()->sum('ending_stock') }} Kg</b></span></h2>
+				    <h2 class="px-1"><span class="badge badge-success p-2">Harga <b>Rp. 8000/Kg</b></span></h2>
+				    <br>
+				    <a class="btn btn-info ml-auto" href="/transaction/create"><i class="fa fa-plus"></i> Pesan kedelai</a>
 {{-- 				    <form class="ml-auto">
 				      <div class="input-group">
 				        <input id="inlineFormInput" type="text" placeholder="Cari" class="form-control">
@@ -38,31 +39,33 @@
 				      </div>
 				    </form> --}}
 			    </div>
-	      	<table class="table table-hover">
-	      	  <thead>
-	      	    <tr>
-	      	      <th>#</th>
-	      	      <th>Petani</th>
-	      	      <th>Stok</th>
-	      	      <th>Tanam</th>
-	      	      <th>Panen</th>
-	      	    </tr>
-	      	  </thead>
-	      	  <tbody>
-	      	  	@foreach (\App\Harvest::where('on_sale', 1)->get() as $harvest)
-	      	  		<tr>
-	      	  		  <th scope="row" class="align-middle">1</th>
-	      	  		  <td>
-	      	  		  	<b class="text-primary">{{ $harvest->onfarm->user->name }}</b><br>
-	      	  		  	{{ $harvest->onfarm->user->poktan->name }} - <b>Yogyakarta</b>
-	      	  		  </td>
-	      	  		  <td class="align-middle">{{ $harvest->ending_stock }} Kg</td>
-	      	  		  <td class="align-middle">{{ $harvest->harvested_at->format('j F Y') }} </td>
-	      	  		  <td class="align-middle">{{ $harvest->onfarm->planted_at->format('j F Y') }}</td>
-	      	  		</tr>
-	      	  	@endforeach
-	      	  </tbody>
-	      	</table>
+			    <div class="table-responsive">
+		      	<table class="table table-hover">
+		      	  <thead>
+		      	    <tr>
+		      	      <th>#</th>
+		      	      <th>Petani</th>
+		      	      <th>Stok</th>
+		      	      <th class="hidden-sm-down">Tanam</th>
+		      	      <th class="hidden-sm-down">Panen</th>
+		      	    </tr>
+		      	  </thead>
+		      	  <tbody>
+		      	  	@foreach (\App\Harvest::where('on_sale', 1)->get() as $harvest)
+		      	  		<tr>
+		      	  		  <th scope="row" class="align-middle">{{ $loop->index+1 }}</th>
+		      	  		  <td>
+		      	  		  	<b class="text-primary">{{ $harvest->onfarm->user->name }}</b><br>
+		      	  		  	{{ $harvest->onfarm->user->poktan->name }} - <b>Yogyakarta</b>
+		      	  		  </td>
+		      	  		  <td class="align-middle">{{ $harvest->ending_stock }} Kg</td>
+		      	  		  <td class="align-middle hidden-sm-down">{{ $harvest->harvested_at->format('j F Y') }} </td>
+		      	  		  <td class="align-middle hidden-sm-down">{{ $harvest->onfarm->planted_at->format('j F Y') }}</td>
+		      	  		</tr>
+		      	  	@endforeach
+		      	  </tbody>
+		      	</table>
+			    </div>
 	    	</div>
 	    </div>
 	  </div>
