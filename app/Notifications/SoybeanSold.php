7d2934bae8daa8a1c748bb3c25826f7286b145b1
w1lldone <wildan.arrahman@gmail.com>
2017-09-23 11:34:54 +0700

    add soybean sold notification

---
+++ app/Notifications/SoybeanSold.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class SoybeanSold extends Notification
+{
+    use Queueable;
+
+    protected $detail;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($detail)
+    {
+        $this->detail = $detail;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+     /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'message' => 'Kedelai anda dibeli',
+            'buyer' => $this->detail->transaction->user->name,
+            'action' => '/sold/'.$this->detail->id,
+        ];
+    }
+}
