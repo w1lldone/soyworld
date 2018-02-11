9db4be1bcbfe5b8c82cc4b6be82b8b6cae7b1a9e
w1lldone <wildan.arrahman@gmail.com>
2018-02-11 23:45:40 +0700

    modify soybean index

--- resources/views/soybean/index.blade.php
+++ resources/views/soybean/index.blade.php
@@ -16,7 +16,7 @@
 		    <div class="row">
 		      <div class="col-12">
 		      	<div class="breadcrumb">
-						  <a href="#" class="h3 breadcrumb-item active">Kedelai</a>
+				  <a href="#" class="h3 breadcrumb-item active">Kedelai</a>
 		      	</div>
 		        <h1 class="text-light" style="font-size: 2rem;">Informasi Stok Kedelai</h1>
 		        <div class="text-muted">
@@ -29,13 +29,17 @@
 		    	<div class="row">
 			    	<div class="col-md-6">
 					    <h2 class="text-primary">{{ $poktan->name }}</h2>
-					    <span class="text-muted"><i class="fa fa-home fa-fw"></i> {{ $poktan->address }}</span>
-					    <span class="text-muted"><i class="fa fa-user fa-fw"></i> {{ $poktan->user->count() }} Petani</span>
+							<span class="text-muted"><i class="fa fa-archive fa-fw"></i> {{ $poktan->active_stock }} kg</span> <br>
+					    <span class="text-muted"><i class="fa fa-home fa-fw"></i> {{ $poktan->address }}</span> <br>
+					    <span class="text-muted"><i class="fa fa-user fa-fw"></i> {{ $poktan->user->count() }} Petani</span> <br>
 			    	</div>
-			    	<div class="col-md-6 text-right">
-					    <a href="{{ route('transaction.create', ['poktan_id' => $poktan->id]) }}" class="btn btn-info"></a>
+			    	<div class="col-md-4 text-center">
+			    	</div>
+			    	<div class="col-md-2 text-right">
+					    <a href="{{ route('transaction.create', ['poktan_id' => $poktan->id]) }}" class="btn btn-success"><i class="fa fa-shopping-cart fa-fw p-0 m-0"> </i> Beli kedelai</a>
 			    	</div>
 			    </div>
+			    <hr>
 		    @endforeach
 		  </div>
 		</section>
