25e858566cb0b02f7d07c8c295e9d6a583814bcc
w1lldone <wildan.arrahman@gmail.com>
2018-02-17 16:51:38 +0700

    add bulk handling

--- app/Http/Controllers/HandlingController.php
+++ app/Http/Controllers/HandlingController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Harvest;
 use Illuminate\Http\Request;
 
 class HandlingController extends Controller
 {
     public function store(Request $request)
     {
-        return $request->all();
+        $this->validate($request, [
+            'id' => 'required|array',
+            'id.*' => 'required|integer|exists:harvests,id',
+            'handling' => 'required|string',
+        ]);
+
+        foreach ($request->id as $id) {
+            $harvest = Harvest::find($id)->postharvest()->create([
+                'name' => $request->handling,
+                'date' => date('Y-m-d'),
+                'cost' => 0,
+            ]);
+        }
+
+        return redirect(route('warehouse.index'))->with('success', "Berhasil menerapkan $request->handling!");
     }
 }
